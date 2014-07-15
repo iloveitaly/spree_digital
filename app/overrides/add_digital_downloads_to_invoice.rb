f931346ad86b37852afdfdf7a29bf11f709a4aab
Michael Bianco <iloveitaly@gmail.com>
2014-07-15 06:02:03 -0400

    Adding :original in deface overrides

--- app/overrides/add_digital_downloads_to_invoice.rb
+++ app/overrides/add_digital_downloads_to_invoice.rb
@@ -1,6 +1,7 @@
 Deface::Override.new(:virtual_path => "spree/shared/_order_details",
                      :name => "add_digital_downloads_to_invoice",
                      :insert_bottom => "td[data-hook='order_item_description']",
+                     :original => 'a264a81b7cda72e468743c41f50b7479283fa4c2',
                      :text => %q{
 <% if order.state == 'complete' and item.variant.digital? %>
   <%= content_tag(:p, :class => 'download_links') do %>
--- app/overrides/add_digital_versions_to_admin_product_tabs.rb
+++ app/overrides/add_digital_versions_to_admin_product_tabs.rb
@@ -1,6 +1,7 @@
 Deface::Override.new(:virtual_path => "spree/admin/shared/_product_tabs",
                      :name => "add_digital_versions_to_admin_product_tabs",
                      :insert_bottom => "[data-hook='admin_product_tabs'], #admin_product_tabs[data-hook]",
+                     :original => '4e3f24e20050cdd7793a86e4a8604a9d6d0b979c',
                      :text => "    <li<%== ' class=\"active\"' if current == \"Digital Versions\" %>>
       <%= link_to t(\"digital_versions\"), admin_product_digitals_path(@product) %>
     </li>
