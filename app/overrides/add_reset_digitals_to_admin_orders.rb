d6d0f73916b32cb418cc352851198acbefc388df
Michael Bianco <iloveitaly@gmail.com>
2014-07-15 09:34:19 -0400

    Fixing reset digitals button

--- app/overrides/add_reset_digitals_to_admin_orders.rb
+++ app/overrides/add_reset_digitals_to_admin_orders.rb
@@ -2,5 +2,5 @@
 Deface::Override.new(:virtual_path => "spree/admin/shared/_order_tabs",
                      :name => "add_reset_digitals_to_admin_orders",
                      :insert_bottom => 'ul[data-hook="admin_order_tabs"]',
-                     :text => %q{ <%= content_tag(:li, button_link_to(t(:reset_downloads), reset_digitals_admin_order_url(@order)), class: 'clear') if @order.digital?%> },
+                     :text => %q{ <%= content_tag(:li, link_to(t(:reset_downloads), reset_digitals_admin_order_url(@order), class: 'text')) if @order.digital?%> },
                      :disabled => false)
